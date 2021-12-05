c77cea693c00018a06f47d14cc7c95c817a14bdb
Raphael G <raphaelgal@gmail.com>
2021-12-05 16:31:38 +0200

    Fixed attachment content disposition header

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/FileController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/FileController.java
@@ -13,9 +13,12 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 
 @Controller
@@ -73,7 +76,7 @@ public class FileController {
     public ResponseEntity<Resource> getFile(int fileId) {
         UserFile file = fileService.getFileByFileId(fileId);
         HttpHeaders headers = new HttpHeaders();
-        headers.set(HttpHeaders.CONTENT_DISPOSITION, "attachment:filename=\"" + file.getFileName());
+        headers.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFileName() + "\"");
         ByteArrayResource resource = new ByteArrayResource(file.getFileData());
 
         return ResponseEntity.ok()
@@ -82,4 +85,49 @@ public class FileController {
                 .contentType(MediaType.parseMediaType(file.getContentType()))
                 .body(resource);
     }
+
+
+        private static final String EXTERNAL_FILE_PATH = "C:/fileDownloadExample/";
+
+//        @RequestMapping("/view/{fileId:.+}")
+//        public void downloadResource(HttpServletRequest request, HttpServletResponse response,
+//                                     @PathVariable("fileId") int fileId) throws IOException {
+//
+//            File file = new File(EXTERNAL_FILE_PATH + fileName);
+//            if (file.exists()) {
+//
+//                //get the mimetype
+//                String mimeType = URLConnection.guessContentTypeFromName(file.getName());
+//                if (mimeType == null) {
+//                    //unknown mimetype so set the mimetype to application/octet-stream
+//                    mimeType = "application/octet-stream";
+//                }
+//
+//                response.setContentType(mimeType);
+//
+//                /**
+//                 * In a regular HTTP response, the Content-Disposition response header is a
+//                 * header indicating if the content is expected to be displayed inline in the
+//                 * browser, that is, as a Web page or as part of a Web page, or as an
+//                 * attachment, that is downloaded and saved locally.
+//                 *
+//                 */
+//
+//                /**
+//                 * Here we have mentioned it to show inline
+//                 */
+//                response.setHeader("Content-Disposition", String.format("inline; filename=\"" + file.getName() + "\""));
+//
+//                //Here we have mentioned it to show as attachment
+//                //response.setHeader("Content-Disposition", String.format("attachment; filename=\"" + file.getName() + "\""));
+//
+//                response.setContentLength((int) file.length());
+//
+//                InputStream inputStream = new BufferedInputStream(new FileInputStream(file));
+//
+//                FileCopyUtils.copy(inputStream, response.getOutputStream());
+//
+//            }
+//        }
+
 }
